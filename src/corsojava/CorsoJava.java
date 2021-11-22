a7705bef62f3e6f078d3f0845537f141b1aeb694
Alessandro Barazzuol <albarazz@gmail.com>
2021-11-22 18:22:59 +0100

    Corso Java v1

--- src/corsojava/CorsoJava.java
+++ src/corsojava/CorsoJava.java
@@ -15,7 +15,7 @@ public class CorsoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Corso Java Alessandro Barazzuol");
+       System.out.println("Corso Java Alessandro Barazzuol v1");
     }
     
 }
